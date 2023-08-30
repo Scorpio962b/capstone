adf4e2991f4cde43fe6956efd8617f0b94ac42e2
Scorpio962b <samaaiab962@gmail.com>
2023-08-30 12:00:22 +0200

    backend host

--- backend/api/controller/controller.js
+++ backend/api/controller/controller.js
@@ -35,18 +35,18 @@ routes.delete('/user/:id', (req, res)=>{
 routes.get('/products', (req, res)=>{
     showProducts(req, res)
 })
-routes.get('/products/:id', (req, res)=>{
+routes.get('/product/:id', (req, res)=>{
     showProductById (req, res)
 })
 routes.post('/products', bodyParser.json(),
     (req, res)=>{
         createProduct(req, res)
     })
-routes.patch('/products/:id', bodyParser.json(),
+routes.patch('/product/:id', bodyParser.json(),
     (req, res)=>{
         updateProduct(req, res)
     })
-routes.delete('/products/:id', (req, res)=>{
+routes.delete('/product/:id', (req, res)=>{
     deleteProduct(req, res)
 })
 
